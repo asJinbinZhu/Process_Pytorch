77ccd6cd58450c716bf303d2009dddb14dae0ac0
asJinbinZhu <jinbinzhu@outlook.com>
2018-04-11 20:43:26 +0800

    test

--- testFullyNet.py
+++ testFullyNet.py
@@ -12,6 +12,8 @@ x = torch.unsqueeze(torch.linspace(-1,1,5),dim=1) # x data(Tensor), shape(100,1)
 y= x.pow(2)+0.2*torch.rand(x.size())
 
 x,y = Variable(x),Variable(y)
+print(x)
+print(y)
 
 class Net(torch.nn.Module):
     def __init__(self,n_features,n_hidden_1,n_hidden_2,n_output):
